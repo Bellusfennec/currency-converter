6164520b1525407646e0b1ebc4a77af5cdfc3ad3
Bellusfennec <bellusfennec@gmail.com>
2024-04-13 14:23:30 +0300

    fix: :bug: fix eslint resolve import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
