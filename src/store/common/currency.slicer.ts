9c9a3e5c4f4629abfcba1a4a7f13f0b92b4d7b2b
Andrey999K <andrey1234xxxx@mail.ru>
2024-04-13 20:32:04 +0300

    fix: delete try and catch in requestCurrency

--- src/store/common/currency.slicer.ts
+++ src/store/common/currency.slicer.ts
@@ -3,12 +3,10 @@ import { Currencies, InitialState } from "../../types";
 import httpService from "../../services/http.services.ts";
 
 export const requestCurrency = createAsyncThunk("currency/set", async () => {
-  try {
-    const currencies = await httpService.get(
-      "https://v6.exchangerate-api.com/v6/b045ab2d173eabfad1494c2b/latest/USD"
-    );
-    return currencies.data.conversion_rates;
-  } catch (e) {}
+  const currencies = await httpService.get(
+    "https://v6.exchangerate-api.com/v6/b045ab2d173eabfad1494c2b/latest/USD"
+  );
+  return currencies.data.conversion_rates;
 });
 
 const setPending = (state: CurrencyState) => {
@@ -18,7 +16,7 @@ const setPending = (state: CurrencyState) => {
 
 const setRejected = (state: CurrencyState) => {
   state.isLoading = false;
-  state.error = null;
+  state.error = true;
 };
 
 type CurrencyState = InitialState<Currencies>;
