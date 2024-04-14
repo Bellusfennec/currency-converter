02b77801de82b6d224cdc9b573fef0bc7f815b64
Dmitry Gerasimov <udeadway523@gmail.com>
2024-04-14 13:25:31 +0300

    fix: fixed Route and added Loader

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -3,16 +3,37 @@ import { Home } from "./pages/Home";
 import { Error } from "./pages/Error";
 import { Favorites } from "./pages/Favorites";
 import { MainLayout } from "./layout/MainLayout";
+import { Suspense } from "react";
+import { Loader } from "./components/common/Loader";
 
 export const AppRoute = () => {
   return (
     <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="*" element={<Error />} />
+      <Route path="/" element={
+        <Suspense fallback={<Loader />}>
+          <MainLayout />
+        </Suspense>
+      }>
+        <Route index element={
+          <Suspense fallback={<Loader />}>
+            <Home />
+          </Suspense>} />
+        <Route path="/favorites" element={
+          <Suspense fallback={<Loader />}>
+            <Favorites />
+          </Suspense>}
+        />
+        <Route path="*" element={
+          <Suspense fallback={<Loader />}>
+            <Error />
+          </Suspense>
+        } />
       </Route>
-      <Route path="*" element={<Error />} />
+      <Route path="*" element={
+        <Suspense fallback={<Loader />}>
+          <Error />
+        </Suspense>
+      }/>
     </Routes>
   );
 };
