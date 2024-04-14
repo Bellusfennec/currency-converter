2c905943380a7d6cf33347d0a7ddf5cf2724f54d
StanislavLabanov <102102899+StanislavLabanov@users.noreply.github.com>
2024-04-14 22:22:31 +0300

    fix: result component props and hendler

--- src/components/ui/Result/Result.tsx
+++ src/components/ui/Result/Result.tsx
@@ -17,6 +17,9 @@ export const Result = ({
     (num: number) => {
       const parts = num.toString().split(".");
       parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      if (parts[1]) {
+        parts[1] = parts[1].substring(0, 5)
+      }
       return parts.join(".");
     },
     [amount, course]
